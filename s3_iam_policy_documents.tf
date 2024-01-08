bc292d572862866f4950b06afe2570cbee521d49
BOlimpio <b.olimpio@outlook.com>
2024-01-07 22:01:38 -0300

    fix iam policy errors

--- s3_iam_policy_documents.tf
+++ s3_iam_policy_documents.tf
@@ -1,7 +1,7 @@
 data "aws_iam_policy_document" "private" {
   source_policy_documents = concat(
-    var.custom_iam_s3_policy_statement,
     [
+      var.custom_iam_s3_policy_statement,
       var.enable_deny_unencrypted_object_uploads ? data.aws_iam_policy_document.deny_unencrypted_object_uploads.*.statement : [],
       var.enable_default_policy ? data.aws_iam_policy_document.default.*.statement : [],
       var.enable_restricted_bucket_access ? data.aws_iam_policy_document.access_control.*.statement : [],
